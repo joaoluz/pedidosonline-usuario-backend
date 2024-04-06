a61fc657a86178b197a3bc7a21fce7d3e07694d7
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-06 09:56:36 -0300

    Implementado validação de dados

--- src/main/java/com/pedidosonline/usuario/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/pedidosonline/usuario/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,22 +1,62 @@
 package com.pedidosonline.usuario.controllers.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import com.pedidosonline.usuario.entites.Usuario;
 import com.pedidosonline.usuario.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
-        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(NoResourceFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), "Url não Encontrado",
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    // @ExceptionHandler(MethodArgumentNotValidException.class)
+    // public ResponseEntity<StandardError> objectNotFound(MethodArgumentNotValidException ex,
+    //         HttpServletRequest request) {
+
+    //     System.out.println(ex.getGlobalError().getDefaultMessage());
+
+    //     StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
+    //             request.getRequestURI());
+    //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    // }
+
+    public void validation() {
+
+        Usuario usuario = new Usuario();
+
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+
+        for (ConstraintViolation<Usuario> violation : violations) {
+            System.out.println("erro:" + violation.getMessage());
+        }
+    }
+
 }
--- src/main/java/com/pedidosonline/usuario/entites/Usuario.java
+++ src/main/java/com/pedidosonline/usuario/entites/Usuario.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -15,6 +16,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "usuario", schema = "usuario")
@@ -27,19 +31,37 @@ public class Usuario implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
     private Integer idUsuario;
+
+    @NotEmpty(message = "Campo NOME obrigatório")
+    @Length(min = 3, max = 100, message = "O nome deve conter entre 3 e 100 caracteres")
     @Column(name = "no_usuario")
     private String noUsuario;
-    @Email
+
+    @Email(message = "Email incorreto")
+    @NotEmpty(message = "Campo E-MAIL obrigatório")
+    //@Column(unique = true) descomentar após os testes, incluir unique no atributo cpf
     private String email;
+
+    @NotEmpty(message = "Campo SENHA obrigatório")
+    @Length(min = 6, message = "Insira no mínimo 6 caracteres")
     private String senha;
+
     @Column(name = "dt_aniversario")
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dtAniversario;
+
+    @NotNull(message = "Campo DDD obrigatório")
+    @Max(value = 99, message = "DDD inválido")
     @Column(name = "nr_ddd")
     private Integer nrDdd;
+
+    @NotNull(message = "Campo NÚMERO obrigatório")
+    //@Max(value = 9, message = "Número de telefone deve conter 9 dígitos")
     @Column(name = "nr_telefone")
     private Integer nrTelefone;
-    @CPF
+
+    @CPF(message = "CPF incorreto")
+    @NotEmpty(message = "Campo CPF obrigatório")
     @Column(name = "nr_cpf")
     private String nrCpf;
 
