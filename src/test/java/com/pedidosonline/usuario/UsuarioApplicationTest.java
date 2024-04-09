1b3421165b84ad9138250e8dd0da6b95fd8ab56c
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-09 10:33:02 -0300

    Teste para Classe principal

---
+++ src/test/java/com/pedidosonline/usuario/UsuarioApplicationTest.java
@@ -0,0 +1,14 @@
+package com.pedidosonline.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioApplicationTest {
+    
+    @Test
+    public void main() {
+        UsuarioApplication.main(new String[] {});
+    }
+
+}
