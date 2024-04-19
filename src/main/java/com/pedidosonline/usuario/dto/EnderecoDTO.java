6e4da366dec5bb84e25948c4d30ffcf93c3c038b
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-19 18:21:56 -0300

    Criada classe EnderecoDTO

---
+++ src/main/java/com/pedidosonline/usuario/dto/EnderecoDTO.java
@@ -0,0 +1,34 @@
+package com.pedidosonline.usuario.dto;
+
+import com.pedidosonline.usuario.entites.Endereco;
+
+public class EnderecoDTO {
+
+    private Integer idEndereco;
+    private String cep;
+
+    
+    public EnderecoDTO(Endereco endereco) {
+        this.idEndereco = getIdEndereco();
+        this.cep = getCep();
+    }
+
+    public Integer getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Integer idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    
+
+    
+}
