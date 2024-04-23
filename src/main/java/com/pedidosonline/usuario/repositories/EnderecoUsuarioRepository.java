218428a258b9214cb5b4d73d2a39e60564bdad84
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-22 21:17:57 -0300

    Criada interface EnderecoUsuarioRepository

---
+++ src/main/java/com/pedidosonline/usuario/repositories/EnderecoUsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.pedidosonline.usuario.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.pedidosonline.usuario.entites.Endereco;
+import com.pedidosonline.usuario.entites.EnderecoUsuario;
+
+public interface EnderecoUsuarioRepository extends JpaRepository<EnderecoUsuario, Integer> {
+
+    //Criar a query
+    @Query("SELECT eu.endereco FROM EnderecoUsuario eu WHERE eu.usuario.idUsuario = :idUsuario")
+    List<Endereco> findAllByUsuario(@Param("idUsuario")Integer idUsuario);
+
+}
